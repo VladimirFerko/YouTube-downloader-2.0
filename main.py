25656be26db4417e2e9b6fee44a760018ca676e6
Vladimir Ferko <ferkovladimir24@gmail.com>
2022-02-23 14:45:47 +0100

    changing directory done, and adding an optional loop

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-import pytube
+from pytube import YouTube
+import os
+
+
+
+
+if __name__ == '__main__':
+
+    # continuing variable
+    continue_var = 'Y'
+
+    # changing directory 
+    os.chdir(os.path.join('/', 'home', 'vladko', 'Documents', 'python_projects', 'YouTube Downloader 2.0', 'data'))
+
+    # getting an input from user (youtube link)
+    while continue_var == 'Y':  
+        song_link = input('Insert a link here: ')
+        YouTube(song_link)
+
+
+        # asking if user want to continue in the loop
+        while True:
+            continue_var = input('Do you want to continue dowloading? [Y/n] ').upper()
+            if continue_var == 'Y' or continue_var == 'N':
+                break
 
