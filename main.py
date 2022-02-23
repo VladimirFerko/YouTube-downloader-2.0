223f17643774f2499e09c0969084ab267c7913b3
Vladimir Ferko <ferkovladimir24@gmail.com>
2022-02-23 17:26:50 +0100

    project done

--- main.py
+++ main.py
@@ -2,19 +2,25 @@ from pytube import YouTube
 import os
 
 
-if __name__ == '__main__':
-
+def main():
     # continuing variable
     continue_var = 'Y'
 
     # changing directory 
     os.chdir(os.path.join('/', 'home', 'vladko', 'Documents', 'python_projects', 'YouTube Downloader 2.0', 'data'))
 
-    # getting an input from user (youtube link)
+
+    # downloading loop
     while continue_var == 'Y':  
-        song_link = input('Insert a link here: ')
+        # downloading video
         try: 
-            YouTube(song_link)
+            dwl = YouTube(input('Insert a link here: '))
+            stream = dwl.streams.filter(only_audio= True).first()
+            output = stream.download()
+
+            base, ext = os.path.splitext(output)
+            os.rename(output, base + '.mp3' )
+
         except: 
             print('Not a valid link..') 
 
@@ -25,3 +31,20 @@ if __name__ == '__main__':
             if continue_var == 'Y' or continue_var == 'N':
                 break
 
+
+# func for moving files
+def move_files():
+    while True:
+        path = input('Type path here where you want to move your files, if not just type "nope": ')
+        if path == 'nope':
+            break
+        elif not os.path.isdir(path):
+            continue
+
+        break
+
+    os.system(f'mv *.mp3 "{path}"')
+
+if __name__ == '__main__':
+    main()
+    move_files()
